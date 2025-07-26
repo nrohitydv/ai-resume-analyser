2053581ec188a18fa4fc933c2319e774c7c282a9
nrohitydv <nrohityadavv@gmail.com>
2025-07-26 13:51:05 +0545

    Enhance ResumeCard component to load resume images dynamically and improve UI feedback for empty resume states in Home route

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
 function ResumeCard({ resume }: { resume: Resume }) {
+  const { fs } = usePuterStore();
+  const [resumeURl, setResumeURl] = useState("");
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(resume.imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeURl(url);
+    };
+
+    loadResume();
+  }, [resume.imagePath]);
   return (
     <Link
       to={`/resume/${resume.id}`}
@@ -10,26 +24,35 @@ function ResumeCard({ resume }: { resume: Resume }) {
     >
       <div className="resume-card-header">
         <div className="flex flex-col gap-2">
-          <h2 className="!text-black font-bold break-words">
-            {resume.companyName}
-          </h2>
-          <h3 className="text-lg break-words text-gray-500">
-            {resume.jobTitle}
-          </h3>
+          {resume.companyName && (
+            <h2 className="!text-black font-bold break-words">
+              {resume.companyName}
+            </h2>
+          )}
+          {resume.jobTitle && (
+            <h3 className="text-lg break-words text-gray-500">
+              {resume.jobTitle}
+            </h3>
+          )}
+          {!resume.companyName && !resume.jobTitle && (
+            <h2 className="!text-black font-bold">Resume</h2>
+          )}
         </div>
         <div className="flex-shrink-0">
           <ScoreCircle score={resume.feedback.overallScore} />
         </div>
       </div>
-      <div className="gradient-border animate-in fade-in duration-1000">
-        <div className="w-full h-full">
-          <img
-            src={resume.imagePath}
-            alt="resume"
-            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-          />
+      {resumeURl && (
+        <div className="gradient-border animate-in fade-in duration-1000">
+          <div className="w-full h-full">
+            <img
+              src={resumeURl}
+              alt="resume"
+              className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+            />
+          </div>
         </div>
-      </div>
+      )}
     </Link>
   );
 }
